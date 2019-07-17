1fa36ac50a1bb9112614fac77207e2c136c9601b
weitangzhu <wei.tangzhu@kuyun.com>
2019-07-17 13:14:00 +0800

    实现：错误处理

--- config.py
+++ config.py
@@ -4,8 +4,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
     # Flask-SQLAlchemy 插件从 SQLALCHEMY_DATABASE_URI 配置变量中获取应用的数据库的位置
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     # 设置数据发生变更之后是否发送信号给应用
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 错误通知邮件配置
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['hhh1230937@163.com']
